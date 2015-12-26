d0aae71269384753dea1cfa871605b294b1b9ef3
Jeremy Yen <jeremy5189@gmail.com>
2015-12-26 22:23:24 +0800

    Add indent

--- db.js
+++ db.js
@@ -2,26 +2,26 @@ module.exports = {
 
     insert: function(config, data, table, datetime) {
 
-	var mysql      = require('mysql');
-	var connection = mysql.createConnection({
-	  host     : config.host,
-	  user     : config.user,
-	  password : config.pass,
-	  database : config.db
-	});
- 
-	connection.connect();
- 	var sql = 'INSERT INTO `' + table + '` (`datetime`, `CNY`, `GBP`, `HKD`, `JPY`, `THB`, `TWD`, `USD`) VALUES (';
-	sql += "'" + datetime + "'";
-	for( var i in data ) {
-	  sql += ",'" + data[i] + "'"; 
-	}
-	sql += ');';
- 	console.log(sql);
-	connection.query(sql, function(err, rows, fields) {
-	  if (err) throw err;
-	});
- 
-	connection.end();
+    	var mysql      = require('mysql');
+    	var connection = mysql.createConnection({
+    	  host     : config.host,
+    	  user     : config.user,
+    	  password : config.pass,
+    	  database : config.db
+    	});
+
+    	connection.connect();
+     	var sql = 'INSERT INTO `' + table + '` (`datetime`, `CNY`, `GBP`, `HKD`, `JPY`, `THB`, `TWD`, `USD`) VALUES (';
+    	sql += "'" + datetime + "'";
+    	for( var i in data ) {
+    	  sql += ",'" + data[i] + "'";
+    	}
+    	sql += ');';
+     	console.log(sql);
+    	connection.query(sql, function(err, rows, fields) {
+    	  if (err) throw err;
+    	});
+
+    	connection.end();
     }
 }
