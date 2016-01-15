3dc735001c6b25706c7ffb24e3599faa125e2998
Jeremy Yen <jeremy5189@gmail.com>
2016-01-15 14:52:09 +0800

    Space bug fixed

--- crontab-gen.js
+++ crontab-gen.js
@@ -4,7 +4,7 @@ var config = require('./config.json'),
 for( var cur in config.map.visa) {
     console.log('\n# PayWhich ' + cur);
     console.log(min + ' 0,6,12,18 * * * /usr/bin/node ' + config.path + 'visa.js 0 ' + cur);
-    console.log(min + ' 0,6,12,18* * * /usr/bin/node ' + config.path + 'master.js 1 ' + cur);
+    console.log(min + ' 0,6,12,18 * * * /usr/bin/node ' + config.path + 'master.js 1 ' + cur);
     //console.log(min + ' 0,6,12,18 * * * /usr/bin/node ' + config.path + 'jcb.js 0 ' + cur);
     min++;
 }
