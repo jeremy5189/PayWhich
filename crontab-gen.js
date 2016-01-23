82153c66fe0cf7f9f5cf0b273094d0d116d438b7
Jeremy Yen <jeremy5189@gmail.com>
2016-01-23 14:14:07 +0800

    Improve crontab gen

--- crontab-gen.js
+++ crontab-gen.js
@@ -11,21 +11,39 @@
 
 var config = require('./config.json'),
     min = 0,
-    _headless = false;
+    _headless = false,
+    _cron = true;
+
+if( process.argv[2] == '--help') {
+    console.log('node crontab-gen.js [--headless] [--nocron]');
+    process.exit(0);
+}
 
 if( process.argv[2] == '--headless') {
     _headless = true;
 }
 
-for( var cur in config.map.visa) {
-    console.log('\n# PayWhich ' + cur);
-    console.log(min + ' 0,6,12,18 * * * ' + config.node_bin + ' ' + config.path + 'visa.js 0 ' + cur);
-
-    if( _headless)
-        console.log(min + ' 0,6,12,18 * * * ' + config.phantomjs_bin + ' ' + config.path + 'visa-headless.js 0 ' + cur);
-    else
-        console.log(min + ' 0,6,12,18 * * * ' + config.node_bin + ' ' + config.path + 'master.js 1 ' + cur);
-
-    //console.log(min + ' 0,6,12,18 * * * /usr/bin/node ' + config.path + 'jcb.js 0 ' + cur);
-    min++;
+if( process.argv[3] == '--nocron' ) {
+    _cron = false;
 }
+
+for( var cur in config.map.visa) {
+
+    var cron = '';
+    if( _cron ) {
+        cron = min + ' 0,6,12,18 * * * ';
+    }
+
+    console.log('\n# PayWhich ' + cur);
+    console.log(cron + config.node_bin + ' ' + config.path + 'master.js 1 ' + cur);
+
+    if( _headless) {
+        cron = min + ' 0,12 * * * ';
+        console.log(cron + config.phantomjs_bin + ' ' + config.path + 'visa-headless.js 0 ' + cur);
+    }
+    else {
+        console.log(cron + config.node_bin + ' ' + config.path + 'visa.js 0 ' + cur);
+    }
+
+    min++;
+ }
