5f5c8515c169cb219e53cabe653a6552aae0818f
Jeremy Yen <jeremy5189@gmail.com>
2016-01-24 12:02:54 +0800

    Improve JSONP script

---
+++ jsonp/config.sample.php
@@ -0,0 +1,4 @@
+<?php
+define( 'CONNECT_STR', "mysql:host=localhost;dbname=paywhich" );
+define( 'DB_USER', 'paywhich' );
+define( 'DB_PASS', '' );
--- jsonp/index.php
+++ jsonp/index.php
@@ -1,17 +1,25 @@
 <?php
 header("Content-Type: application/json");
 date_default_timezone_set("Asia/Taipei");
-include('../../include/config.php');
+include('config.php');
 
+// Initial JSON array
 $json = array(
     'visa' => array(
-        'date' => null
+        'date'   => null,
+        'length' => 0
     ),
     'mastercard' => array(
-        'date' => null
+        'date'   => null,
+        'length' => 0
+    ),
+    'jcb' => array(
+        'date'   => null,
+        'length' => 0
     )
 );
 
+// Deal with visa and mastercard first
 $link = new PDO(CONNECT_STR, DB_USER, DB_PASS);
 $list = array('visa', 'mastercard');
 
@@ -21,14 +29,39 @@ foreach($list as $int_org) {
     $sth = $link->prepare($sql);
     $sth->execute(array($int_org));
 
+
     while( $result = $sth->fetch(PDO::FETCH_OBJ) ) {
 
         $json[$int_org][$result->base_currency] = array(
-            'NTD' => $result->TWD
+            'NTD' => floatval($result->TWD)
         );
 
         $json[$int_org]['date'] = $result->settle_date;
+        $json[$ini_org]['length']++;
     }
 }
+
+// Then deal with JCB
+$sql = "SELECT * FROM `paywhich` WHERE `int_org` = 'jcb' ORDER BY `created_at` DESC LIMIT 1";
+$sth = $link->prepare($sql);
+$sth->execute(array());
+$result = $sth->fetch(PDO::FETCH_OBJ);
+$data = json_decode($result->json);
+
+foreach( $data as $cur => $val ) {
+
+    // Skip TWD
+    if( $cur == 'TWD' )
+        continue;
+
+    $json['jcb'][$cur] = array(
+        'USD' => floatval($val),
+        'NTD' => floatval($data->TWD) / floatval($val) // Using NTD
+    );
+
+    $json[$ini_org]['length']++;
+}
+
+$json['jcb']['date'] = $result->settle_date;
 
 echo $_GET['callback'] . '(' . json_encode($json) . ')';
